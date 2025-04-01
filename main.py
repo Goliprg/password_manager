8aecb39e0f89da652976b65946f2f4d1041ce234
goliprg <golnoush.hosseini@gmail.com>
2025-04-01 20:33:38 -0230

    "Initial commit"

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from idlelib.editor import keynames
 from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -34,21 +36,49 @@ def save():
     website = website_entry.get()
     username = username_entry.get()
     password = password_entry.get()
+    new_data = {
+        website:{
+            "username":username,
+            "password":password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Error!", message= "Please fill up all of the fields")
     else:
         save_ok = messagebox.askokcancel(title="Approve",message=f"These are the details entered:\nwebsite: {website}\nusername: {username}\npassword: {password}")
         if save_ok:
-            file = open("data.txt","a")
-            file.write(f"{website} / {username} / {password} \n")
-            file.close()
+            try:
+                with open("data.json","r") as file:
+                    data = json.load(file)
+                    data.update(new_data)
+            except FileNotFoundError:
+                data = new_data
+            finally:
+                with open("data.json","w") as file:
+                    json.dump(data,file,indent=4)
+
             pyperclip.copy(password)
             website_entry.delete(0, END)
             username_entry.delete(0, END)
             username_entry.insert(0, string="andy@gmail.com")
             password_entry.delete(0, END)
             website_entry.focus()
+
+# ---------------------------- SEARCH PASSWORD ------------------------#
+def search_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as file:
+            saved_data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title=website, message="No data file found")
+    else:
+        if website in saved_data:
+            messagebox.showinfo(title=website ,message=f"Email: {saved_data[website]["username"]}\nPassword: {saved_data[website]["password"]}")
+        else:
+            messagebox.showinfo(title=website, message="No details for the website exist")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -69,9 +99,13 @@ username_label.grid(row=2,column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3,column=0)
 
-website_entry = Entry(width=52)
+website_entry = Entry(width=32)
 website_entry.focus()
-website_entry.grid(row=1,column=1,columnspan=2,sticky="w")
+website_entry.grid(row=1,column=1,sticky="w")
+
+search_button = Button(text="Search", width=14,command=search_password)
+search_button.grid(row=1, column=2,sticky="w")
+
 
 username_entry = Entry(width=52)
 username_entry.insert(0,string="andy@gmail.com")
